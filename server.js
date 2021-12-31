989b90766200b2495144f0517753f2a4c9f4141a
LeahFusari <87339742+LeahFusari@users.noreply.github.com>
2021-12-31 11:31:08 -0500

    partials

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ const express = require('express');
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const exphbs = require('express-handlebars');
-const hbs = exphbs.create({});
+const helpers = require('./utils/helpers');
+const hbs = exphbs.create({ helpers });
 const session = require('express-session');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const sess = {
@@ -16,6 +17,8 @@ const sess = {
     db: sequelize
   })
 };
+
+
 
 
 const app = express();
